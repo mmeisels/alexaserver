128f6b00162469fe3209e11e327dfacd7315cf3c
Michael Meisels <mmeisels@salesforce.com>
2016-08-19 13:38:31 +1000

    Commited Code

--- apps/sprk-intro/index.js
+++ apps/sprk-intro/index.js
@@ -17,7 +17,7 @@ app.error = function( exception, request, response ) {
 	response.say( 'Sorry an error occured ' + error.message);
 };
 
-app.intent('sayNumber',
+app.intent('sayHeroku',
   {
     "slots":{"name":"AMAZON.US_FIRST_NAME"}
 	,"utterances":[
