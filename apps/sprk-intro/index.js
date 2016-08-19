50f62d0ecbfb2328481a563a302dcc8bff5bccd4
Michael Meisels <mmeisels@salesforce.com>
2016-08-19 13:25:24 +1000

    Commited Code

---
+++ apps/sprk-intro/index.js
@@ -0,0 +1,33 @@
+module.change_code = 1;
+'use strict';
+
+var alexa = require( 'alexa-app' );
+var app = new alexa.app( 'sprk-intro' );
+
+
+app.launch( function( request, response ) {
+	response.say( 'Welcome to the Salesforce Intro' ).reprompt( 'This works nicely.' ).shouldEndSession( false );
+} );
+
+
+app.error = function( exception, request, response ) {
+	console.log(exception)
+	console.log(request);
+	console.log(response);
+	response.say( 'Sorry an error occured ' + error.message);
+};
+
+app.intent('sayNumber',
+  {
+    "slots":{"name":"AMAZON.US_FIRST_NAME"}
+	,"utterances":[
+		"tell me where you are running {name}"
+		]
+  },
+  function(request,response) {
+    var name = request.slot('name');
+    response.say("Hi, my name is Alexa. I am running on Heroku. Thanks " + name);
+  }
+);
+
+module.exports = app;
