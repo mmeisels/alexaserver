494e8511d58bd1d80659c757b3736b01ab72fd98
Michael Meisels <mmeisels@salesforce.com>
2016-08-20 20:32:42 +1000

    Commited Code

--- apps/sprk-intro/index.js
+++ apps/sprk-intro/index.js
@@ -68,9 +68,11 @@ app.intent('sayHeroku',
 						});
 		    	}
 				}
+				console.log("done response : ");
+				response.say("Hi, my name is Alexa. I am running on Heroku. We have found a record for Account Name " + accountName + ".  "+ accountName + " has total Opportunity Amount value of " + opptyAmount +".");
+				response.send();
 		});
-		console.log("done response : ");
-		response.say("Hi, my name is Alexa. I am running on Heroku. We have found a record for Account Name " + accountName + ".  "+ accountName + " has total Opportunity Amount value of " + opptyAmount +".").send();
+		return false;
   }
 );
 
