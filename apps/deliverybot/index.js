b680976378da6bcacb6e3f28e5f97dd5632c342a
Michael Meisels <mmeisels@salesforce.com>
2016-08-22 19:36:49 +1000

    Added Drone code

--- apps/deliverybot/index.js
+++ apps/deliverybot/index.js
@@ -28,44 +28,8 @@ conn.login('mmeisels@demo.trolleyapp1', 'demopassw0rd8JMXR8jIgqdyVAGpyFEA238f', 
   console.log("Org ID: " + userInfo.organizationId);
 });
 
-app.launch( function( request, response ) {
+app.launch(function( request, response ) {
   response.say( 'Welcome to the The Drone Machine' ).reprompt( 'This works nicely.' ).shouldEndSession( false );
 });
-
-
-app.intent('Land',
-  {
-    "slots":{"name":"AMAZON.US_FIRST_NAME"},
-    "utterances":[
-		"Land the drone",
-		"Stop",
-		"Sleep",
-		"Go Down",
-    "Land"
-		]
-  },
-	function(request,response) {
-    var landMessage = {
-            "command" : "land",
-            "sessionId" : session.sessionId
-    };
-    console.log(pubnub.get_version());
-    //response.setShouldEndSession(true);
-    pubnub.publish({
-        channel   : 'my_channel',
-        message   : landMessage,
-        callback  : function(e) {
-             console.log( "SUCCESS!", e );
-             response.say("Drone is going down");
-             response.send();
-        },
-        error  : function(e) {
-        response.say("Can not connect to Drone");
-        response.send();
-        console.log( "FAILED! RETRY PUBLISH!", e ); }
-    });
-		return false;
-  }
-);
 
 module.exports = app;
