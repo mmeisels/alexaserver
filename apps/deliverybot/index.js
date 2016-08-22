0db096207451c1d96b3a9a5650704fd8e4ffae9f
Michael Meisels <mmeisels@salesforce.com>
2016-08-22 19:34:33 +1000

    Added Drone code

--- apps/deliverybot/index.js
+++ apps/deliverybot/index.js
@@ -28,6 +28,11 @@ conn.login('mmeisels@demo.trolleyapp1', 'demopassw0rd8JMXR8jIgqdyVAGpyFEA238f', 
   console.log("Org ID: " + userInfo.organizationId);
 });
 
+app.launch( function( request, response ) {
+  response.say( 'Welcome to the The Drone Machine' ).reprompt( 'This works nicely.' ).shouldEndSession( false );
+});
+
+
 app.intent('Land',
   {
     "slots":{"name":"AMAZON.US_FIRST_NAME"},
@@ -58,78 +63,6 @@ app.intent('Land',
         response.say("Can not connect to Drone");
         response.send();
         console.log( "FAILED! RETRY PUBLISH!", e ); }
-    });
-		return false;
-  }
-);
-
-app.intent('TakeOff',
-  {
-    "slots":{"name":"AMAZON.US_FIRST_NAME"},
-    "utterances":[
-		"Take Off",
-		"Fly",
-		"Fly the drone",
-		"Hover"
-    "Go Up"
-    "Blink"
-		]
-  },
-	function(request,response) {
-    var takeOffmessage = {
-             "command" : "takeOff",
-             "sessionId" : session.sessionId
-    };
-    console.log(pubnub.get_version());
-    pubnub.publish({
-         channel   : 'my_channel',
-         message   : takeOffmessage,
-         callback  : function(e) {
-              console.log( "SUCCESS!", e );
-              response.say("Drone is flying");
-              response.send();
-         },
-         error     : function(e) {
-              response.say("Could not connect to Drone");
-              response.send();
-             console.log( "FAILED! RETRY PUBLISH!", e ); }
-     });
-		return false;
-  }
-);
-
-
-app.intent('Initiate',
-  {
-    "slots":{"name":"AMAZON.US_FIRST_NAME"},
-    "utterances":[
-		"Blink",
-		"Initiate Drone",
-		"Initiate",
-		"Start Drone",
-    "Wake up",
-    "Wake up Spider"
-		]
-  },
-	function(request,response) {
-    var initiateMessage = {
-            "command" : "initiate",
-            "sessionId" : session.sessionId
-        };
-        console.log(pubnub.get_version());
-        pubnub.publish({
-          channel   : 'my_channel',
-          message   : initiateMessage,
-          callback  : function(e) {
-          console.log( "SUCCESS!", e );
-          response.say("Drone is ready to fly");
-          response.send();
-        },
-        error     : function(e) {
-          response.say("Could not connect to Drone");
-          response.send();
-          console.log( "FAILED! RETRY PUBLISH!", e );
-        }
     });
 		return false;
   }
