f27183c62e2330afa737aaf33453f834b845a354
Michael Meisels <mmeisels@salesforce.com>
2016-08-22 19:44:07 +1000

    Added Drone code

--- apps/deliverybot/index.js
+++ apps/deliverybot/index.js
@@ -4,13 +4,13 @@ module.change_code = 1;
 var alexa = require( 'alexa-app' );
 var app = new alexa.app( 'deliverbot' );
 var sf = require('node-salesforce');
-
+var pubnub = require("pubnub");
 var conn = new sf.Connection({
   // you can change loginUrl to connect to sandbox or prerelease env.
   // loginUrl : 'https://test.salesforce.com'
 });
 
-var pubnub = require("pubnub")({
+PUBNUB.init({
     ssl           : true,
     publish_key   : "pub-c-46d93d38-de2a-48fa-ba27-11b2d8dcff30",
     subscribe_key : "sub-c-573f0f1e-6828-11e6-8c1f-02ee2ddab7fe"
