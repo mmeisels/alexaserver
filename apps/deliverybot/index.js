1e1c5cb8726482e3f964040f80319a8e24071223
Michael Meisels <mmeisels@salesforce.com>
2016-08-22 21:25:09 +1000

    Added Drone code

--- apps/deliverybot/index.js
+++ apps/deliverybot/index.js
@@ -6,7 +6,7 @@ var app = new alexa.app( 'deliverybot' );
 var sf = require('node-salesforce');
 var pubnub = require("pubnub");
 
-var pn = PUBNUB.init(({
+var pn = PUBNUB.init({
     ssl           : true,
     publish_key   : "pub-c-46d93d38-de2a-48fa-ba27-11b2d8dcff30",
     subscribe_key : "sub-c-573f0f1e-6828-11e6-8c1f-02ee2ddab7fe"
